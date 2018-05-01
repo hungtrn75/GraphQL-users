85ab377b59b56856fef60d6c5d475afd1704b660
hungtrn75 <hungtrn75@gmail.com>
2018-05-01 07:17:01 +0700

    Nested Queries

--- schema/schema.js
+++ schema/schema.js
@@ -16,27 +16,60 @@ const {
 
 const URL_API = 'http://localhost:3000';
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString
+        }
+    }
+});
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
-        id: { type: GraphQLString },
-        firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        id: {
+            type: GraphQLString
+        },
+        firstName: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        },
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`${URL_API}/companies/${parentValue.companyId}`)
+                    .then(res => res.data);
+            }
+        }
     }
 });
+
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         user: {
             type: UserType,
-            args: { id: { type: GraphQLString } },
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
             resolve(parentValue, args) {
                 //return _.find(users, { id: args.id });
                 return axios.get(`${URL_API}/users/${args.id}`).then(res => res.data);
             }
         }
-    }    
+    }
 });
 
 module.exports = new GraphQLSchema({
